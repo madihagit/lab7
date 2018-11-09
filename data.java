6049756e1152ee55a5d42ef0db4a1c678b4f8ca9
madihagit <44543278+madihagit@users.noreply.github.com>
2018-11-09 23:44:06 +0500

    Add files via upload

---
+++ data.java
@@ -0,0 +1,60 @@
+
+public class Person {
+	int id;
+	String name;
+	String fatherName;
+	String organization;
+	String mobile;
+	public int getid()
+	{
+	     
+	    return this.id;
+	}
+	public void setid(int id)
+	{
+	     
+	     this.id = id;
+	}
+
+	
+	public String getname()
+	{
+	     
+	    return this.name;
+	}
+	public void setname(String name)
+	{
+	     
+	     this.name =name;
+	}
+
+	public String getfatherName()
+	{
+	     
+	    return this.fatherName;
+	}
+	public void setfatherName(String fatherName)
+	{
+	     
+	     this.fatherName =fatherName;
+	}
+	public String getOrganization()
+	{
+	     
+	    return this.organization;
+	}
+	public void setOrganization(String organization)
+	{
+	     
+	     this.organization =organization;
+	}
+	public String getmobile()
+	{
+	     
+	    return this.mobile;
+	}
+	public void setmobile(String mobile)
+	{
+	     
+	     this.mobile =mobile;
+	}}
---
+++ main.java
@@ -0,0 +1,69 @@
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class DBMain {
+	
+		public static void main(String[] args) throws SQLException {
+			Scanner input = new Scanner(System.in);
+			boolean loopcounter =true;
+			
+			Person person_record = new Person();
+			DBHandler h1 = new DBHandler();
+			while(loopcounter) {
+				System.out.println("1. Add a record"
+						+ "\n2. Delete record\n"
+						+ "3. update records"
+						+ "4. get all records"
+						+ "\n5. EXIT");
+				
+				System.out.print("Enter from above numbers choice: ");
+				int temp = input.nextInt();
+				
+				switch(temp) {
+				case 1:
+					System.out.print("Enter id: ");
+					int id = input.nextInt();
+					
+					System.out.print("Enter Name: ");
+					String name = input.next();
+					System.out.print("Enter fatherName: ");
+					String fatherName = input.next();
+					
+					System.out.print("Enter organization ");
+					String org = input.next();
+					System.out.print("Enter mobile ");
+					String mob = input.next();
+					
+				
+					
+					
+					
+					person_record.setid(id);
+					person_record.setname(name);
+					person_record.setfatherName(fatherName);
+					person_record.setOrganization(org);
+					person_record.setmobile(mob);
+					h1.save(person_record);
+					break;
+				case 2:
+					h1.delete();
+					break;
+				case 3:
+					h1.update();
+					break;
+				case 4:
+					h1.get();
+					break;
+				case 5:
+					loopcounter = false;
+					break;
+				default:
+					System.out.println("Out of scope number..");
+				}
+			}
+		
+		}
+
+}
